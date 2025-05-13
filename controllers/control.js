643817768ad23782c020ab25704974d56951aa48
mani <manisunny632@gmail.com>
2025-05-12 23:36:37 -0700

    first commit

---
+++ controllers/control.js
@@ -0,0 +1,9 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+const app= express()
+app.use(express.json())
+const port = 5000
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)    
+})
